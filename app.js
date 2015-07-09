0fc3f8583cc8fdbc838515395acfe4fececf118c
alu20alu20 <student10@js-c02mxf7ug083.punahou.edu>
2015-07-09 11:10:32 -1000

    guys

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 var computerChoice;
 function computerThink(){
 	computerChoice = Math.floor(Math.random()*100);
-	if(computerChoice < 1){
-		computerChoice = "Diamond";
-		alert("You Got Diamond");
+	if(computerChoice < 5){
+		computerChoice = Diamond;
+		diamondPoint++;
+			alert("You Got Diamond");
 }
-	else if(computerChoice > 24){
-		computerChoice = "Nothing";
+	else if(computerChoice > 31){
+		computerChoice = Nothing;
+		nothingPoint++;	
 			alert("You Got Nothing");	
 }
-	else if(computerChoice > 2 && computerChoice < 11){
-		computerChoice = "Gold";
+	else if(computerChoice > 5 && computerChoice < 16){
+		computerChoice = Gold;
+		goldPoint++;
 			alert("You Got Gold");	
 }
-	else if(computerChoice > 9 && computerChoice < 26){
-		computerChoice = "Silver";
+	else if(computerChoice > 15 && computerChoice < 31){
+		computerChoice = Silver;
+		silverPoint++;
 			alert("You Got Silver");	
 }
-
 }
 
+Score = diamondPoint*50 + silverPoint*10 + goldPoint*25 + nothingPoint*1;
 
+var Diamond = 50;
 
+var Silver = 10;
 
+var Gold = 25;
+
+var Nothing = 1;
+
+var Uscore = 0;
+
+var press = function(){
+	Uscore++;
+	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+};
 
 
 
